015da05d96aec18755c42b09a74f1eddf021f64e
Afshar Mohebi <mohebi@iranbarnet.com>
2020-06-22 12:56:44 +0430

    done with the  basic boilerplate

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+// controllers
+import users from '../controllers/user.js';
+
+const router = express.Router();
+
+router
+  .post('/login/:userId', (req, res, next) => { });
+
+export default router;
---
+++ server/index.js
@@ -0,0 +1,36 @@
+import http from "http";
+import express from "express";
+import logger from "morgan";
+// routes
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/user.js"
+
+const app = express();
+
+/** Get port from environment and store in Express. */
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
+/** catch 404 and forward to error handler */
+app.use('*', (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: 'API endpoint doesnt exist'
+  })
+});
+
+/** Create HTTP server. */
+const server = http.createServer(app);
+/** Listen on provided port, on all network interfaces. */
+server.listen(port);
+/** Event listener for HTTP server "listening" event. */
+server.on("listening", () => {
+  console.log(`Listening on port:: http://localhost:${port}/`)
+});
